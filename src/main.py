6a0618eae358e5a87caee4496750f959a05d5489
Quentin Prigent <quentin@macbook-pro-de-quentin.home>
2022-09-05 19:21:09 +0200

    Method to run quantum circuit on a real device done

--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
 from services.connection_manager import account_initialization_manager, ConnectionManager
+from qiskit import QuantumCircuit
 
 if __name__ == '__main__':
     account_initialization_manager()
-    connection_manager = ConnectionManager()
-    print(connection_manager.get_to_least_busy_quantum_computer())
+
+    quantum_circuit = QuantumCircuit(3)
+    quantum_circuit.h(0)
+    quantum_circuit.cx(0, [1, 2])
+    quantum_circuit.measure_all()
+    print('Quantum circuit ready.')
+
+    connection_manager = ConnectionManager(real_machine=True)
+    connection_manager.get_least_busy_quantum_computer()
+    quantum_computer = connection_manager.quantum_computer
+    print('Connected to quantum computer:')
+    print(quantum_computer)
+
+    job = connection_manager.run_quantum_circuit(quantum_circuit=quantum_circuit)
+    print(job.result().get_counts())
